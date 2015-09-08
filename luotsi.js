ace616962bedebcdc5b72f6dd7d3ddfd875d7c38
Teemu Heikkilä <teemu.heikkila@reaktor.fi>
2015-09-09 02:42:18 +0300

    hotfix

--- luotsi.js
+++ luotsi.js
@@ -54,7 +54,7 @@ var HOST = '127.0.0.1';
 
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
-
+var client = dgram.createSocket('udp4');
 server.on('listening', function () {
 	var address = server.address();
 	console.log('UDP Server listening on ' + address.address + ":" + address.port);

8807b33d6c607c0d674f7a2e49b5ef72f1a6beb3
Teemu Heikkilä <teemu@emblica.fi>
2016-10-13 23:02:56 +0300

    Refactor luotsi

--- luotsi.js
+++ luotsi.js
@@ -1,10 +1,16 @@
-var Handlebars = require('handlebars');
-
-var fs = require('fs');
-var fetch = require('node-fetch');
-var Promise = require('bluebird');
-var crypto = require('crypto');
+const Handlebars = require('handlebars');
+const winston = require('winston');
+const fs = require('fs');
+const a = require('axios');
+const Promise = require('bluebird');
+const crypto = require('crypto');
 const exec = require('child_process').exec;
+
+const logger = new (winston.Logger)({
+    transports: [
+      new (winston.transports.Console)({'timestamp':true})
+    ]
+});
 
 function md5(data) {
 	return new Promise((resolve, reject) => {
@@ -12,45 +18,58 @@ function md5(data) {
 	});
 }
 
-var checksum = "";
+const VERSION = "0.1.0";
 
-var readFile = Promise.promisify(fs.readFile);
-var writeFile = Promise.promisify(fs.writeFile);
+let checksum = "";
+
+const readFile = Promise.promisify(fs.readFile);
+const writeFile = Promise.promisify(fs.writeFile);
+
+const settings = {
+	stats: {
+		path: "/haproxy",
+		user: process.env.STATS_USER || "hadmin",
+		pass: process.env.STATS_PASS || "hadmin"
+	},
+	maintenance_page: process.env.MAINTENANCE_PAGE || '/etc/haproxy/maintenance.http',
+	cert_path: process.env.CERT_PATH ||'/etc/haproxy/cert.pem',
+
+}
+
+const template_file = 'haproxy.template';
+const config_file = '/etc/haproxy/haproxy.cfg';
+const halti_url = (process.env.HALTI_URL || "http://localhost:4040") + "/api/v1/loadbalancers/config";
 
 
-var template_file = 'haproxy.template';
-var config_file = '/etc/haproxy/haproxy.cfg';
-var halti_url = (process.env.HALTI_URL || "http://10.4.1.224:4040") + "/api/v1/loadbalancers/config";
-
-
-function render_config(template_content, loadbalancers) {
+function render_config(template_content, settings, loadbalancers) {
 	return new Promise((resolve, reject) => {
 		const template = Handlebars.compile(template_content);
-		resolve(template({loadbalancers: loadbalancers}));
+		resolve(template({loadbalancers: loadbalancers, settings: settings}));
 	});
 }
 
 
-
 function reload_haproxy () {
 	return new Promise((resolve, reject) => {
+		logger.info("Reload haproxy");
 		exec("service haproxy reload", (error, stdout, stderr) => resolve());
 	});
 }
 
 
 function check () {
-	Promise.all([readFile(template_file, 'utf-8'), fetch(halti_url).then(res => res.json())])
+	Promise.all([readFile(template_file, 'utf-8'), settings, a.get(halti_url).then(res => res.data)])
 	.spread(render_config)
 	.then(rendered_config => Promise.join(md5(rendered_config), rendered_config))
 	.spread((hash, rendered_config) => {
 		if (hash !== checksum) {
 			checksum = hash;
-			console.log("Update!", hash);
+			logger.info("Update haproxy config, new checksum: %s", hash);
 			return writeFile(config_file, rendered_config)
 							.then(reload_haproxy)
 		}
-	}).catch(e => console.log(e))
+	}).catch(e => logger.error(e))
 }
+logger.info("Halti - Luotsi loadbalancer v%s", VERSION)
 
 setInterval(check, 5000);
